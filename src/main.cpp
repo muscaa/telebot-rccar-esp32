ca2603926b43cb3b0deaafe1c8b42897cad0e51e
Musca Mihail <muscamihaill@gmail.com>
2025-06-12 19:35:16 +0300

    switch back to arduino only, added esp32-dualsense library, added blink test, refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include <Arduino.h>
-#include <esp_log.h>
+#include <ps5Controller.h>
 
-extern "C" void app_main() {
-    initArduino();
-
+void setup() {
+    Serial.begin(115200);
+    ps5.begin("88:03:4C:22:F9:CC");
+    
     pinMode(2, OUTPUT);
+}
 
-    ESP_LOGI("app_main", "Starting Blink Example");
-
-    while (true) {
-        digitalWrite(2, HIGH);
-        delay(1000);
-        digitalWrite(2, LOW);
+void loop() {
+    while (!ps5.isConnected()) {
+        Serial.println("Waiting for PS5 controller to connect...");
         delay(1000);
     }
+
+    Serial.println("PS5 controller connected!");
+
+    while (ps5.isConnected()) {
+        if (ps5.Square()) {
+            digitalWrite(2, HIGH);
+        } else {
+            digitalWrite(2, LOW);
+        }
+    }
+
+    Serial.println("PS5 controller disconnected!");
 }
